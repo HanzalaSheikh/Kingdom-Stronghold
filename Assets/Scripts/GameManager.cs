3aa53fd7a3e744e9968a9c0b2f9a57295896e781
Muhammad Hanzala <muhammadhanzala214@gmail.com>
2024-09-16 21:32:36 +0500

    Build file created

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,181 @@
+// using UnityEngine;
+// using TMPro;
+
+// public class GameManager : MonoBehaviour
+// {
+//     public TextMeshProUGUI scoreText;
+//     public TextMeshProUGUI goldText;
+
+//     public int score = 0;
+//     private int gold = 300;
+
+//     public static GameManager instance;
+
+//     void Awake()
+//     {
+//         if (instance == null)
+//         {
+//             instance = this;
+//             DontDestroyOnLoad(gameObject);
+//         }
+//         else
+//         {
+//             Destroy(gameObject);
+//         }
+//     }
+
+//     public void AddScore(int points)
+//     {
+//         score += points;
+//         UpdateScoreText();
+//     }
+
+//     public void AddGold(int coins)
+//     {
+//         gold += coins;
+//         UpdateGoldText();
+//     }
+
+//     public bool CanAfford(int cost)
+//     {
+//         return gold >= cost;
+//     }
+
+//     public void SpendGold(int cost)
+//     {
+//         if (CanAfford(cost))
+//         {
+//             gold -= cost;
+//             UpdateGoldText();
+//         }
+//         else
+//         {
+//             Debug.LogWarning("Not enough gold to buy the tower.");
+//         }
+//     }
+
+//     public void UpdateScoreText()
+//     {
+//         scoreText.text = "Score: " + score.ToString();
+//     }
+
+//     private void UpdateGoldText()
+//     {
+//         goldText.text = "Gold: " + gold.ToString();
+//     }
+
+// public void Reset()
+// {
+//     score = 0;
+//     gold = 200; // Reset to initial gold value
+//     UpdateScoreText();
+//     UpdateGoldText();
+// }
+
+
+//     // This is the new method to get the current score
+//     public int GetScore()
+//     {
+//         return score;
+//     }
+// }
+
+
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText; // Make sure this is assigned in the Inspector or dynamically
+    public TextMeshProUGUI goldText;
+
+    public int score = 0;
+    private int gold = 300;
+
+    public static GameManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        // Reinitialize the UI references when the scene is restarted
+        scoreText = GameObject.Find("ScoreText").GetComponent<TextMeshProUGUI>();
+        goldText = GameObject.Find("GoldText").GetComponent<TextMeshProUGUI>();
+
+        UpdateScoreText();
+        UpdateGoldText();
+    
+    
+   
+    }
+
+    public void AddScore(int points)
+    {
+        score += points;
+        UpdateScoreText();
+    }
+
+    public void AddGold(int coins)
+    {
+        gold += coins;
+        UpdateGoldText();
+    }
+
+    public bool CanAfford(int cost)
+    {
+        return gold >= cost;
+    }
+
+    public void SpendGold(int cost)
+    {
+        if (CanAfford(cost))
+        {
+            gold -= cost;
+            UpdateGoldText();
+        }
+        else
+        {
+            Debug.LogWarning("Not enough gold to buy the tower.");
+        }
+    }
+
+    public void UpdateScoreText()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + score.ToString();
+        }
+    }
+
+    private void UpdateGoldText()
+    {
+        if (goldText != null)
+        {
+            goldText.text = "Gold: " + gold.ToString();
+        }
+    }
+
+    public void Reset()
+    {
+        score = 0;
+        gold = 200; // Reset to initial gold value
+        UpdateScoreText();
+        UpdateGoldText();
+    }
+    public int GetScore()
+{
+    return score;  // Return the current score
+}
+
+}
